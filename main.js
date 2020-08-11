0a95ab889c59b4fd9014e92e236a9718c2b46ffd
Abdulateef Al-radaee <abdulateef.radaee@gmail.com>
2020-08-11 17:24:30 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
-let modal = select(undefined, 'modal');
+let modal = document.;
 let heart = select(undefined, undefined, 'like');
 
 
